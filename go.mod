56f8ae372d97b09f4e4c507045010f01b4cb1b5f
pankaj-889 <pankajsingh85201@gmail.com>
2024-07-07 15:21:48 +0530

    apis using mux router and PG db

---
+++ go.mod
@@ -0,0 +1,9 @@
+module go-postgres-stocks-yt
+
+go 1.22.0
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.3.0
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	// fs := http.FileServer(http.Dir("build"))
+	// http.Handle("/", fs)
+	fmt.Println("Starting server on the port 8080...")
+
+	log.Fatal(http.ListenAndServe(":6960", r))
+}
